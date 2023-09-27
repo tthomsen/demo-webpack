858cf4b9922511189d82bd19e9d42c21b31cc533
Travis Thomsen <travis.n.thomsen@gmail.com>
2023-09-27 10:58:56 -0500

    add webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    future: {
+      webpack5: true,
+    },
+    webpack: function (config, options) {
+      console.log(options.webpack.version); // Should be webpack v5 now
+      config.experiments = {};
+      return config;
+    },
+  };
