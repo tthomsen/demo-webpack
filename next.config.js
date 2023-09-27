7d3ba08262615205d2813b455dfcec45b4be2fd9
Travis N. Thomsen <tthomsen@binaryvoid.com>
2023-09-27 14:42:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    future: {
-      webpack5: true,
-    },
-    webpack: function (config, options) {
-      console.log(options.webpack.version); // Should be webpack v5 now
-      config.experiments = {};
-      return config;
-    },
-  };
+module.exports = nextConfig
