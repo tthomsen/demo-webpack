a5046d1f31b48cdc7cf6597544c6bb5a640df1f0
Travis Thomsen <travis.n.thomsen@gmail.com>
2023-10-12 12:10:06 -0500

    add tests

---
+++ __tests__/Home.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/app/page'
+
+describe('Home', () => {
+    it('should have Docs text', () => {
+        render(<Home />);
+
+        const getElm = screen.getByText('Docs');
+
+        expect(getElm).toBeInTheDocument();
+    });
+
+    it('should containe the text "Lorem ipsum"', () => {
+        render(<Home />);
+
+        const getElm = screen.getByText(/Lorem ipsum/i);
+
+        expect(getElm).toBeInTheDocument();
+    });
+
+    it('should have a heading', () => {
+        render(<Home />);
+
+        const heading = screen.getByHeading('heading', {
+            name: 'Learn <span>-&gt;</span>'
+        });
+
+        expect(heading).toBeInTheDocument();
+    });
+});
